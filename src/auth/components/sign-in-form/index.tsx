e19a0f0e3f9c407d441786ce8e8021cd7a7c0696
Lucas A. Camacho <111870284+LcsCamacho@users.noreply.github.com>
2023-12-09 22:30:05 -0300

    ajustes

--- src/auth/components/sign-in-form/index.tsx
+++ src/auth/components/sign-in-form/index.tsx
@@ -2,9 +2,9 @@
 import { FormEvent, useRef } from "react";
 import { AuthService } from "../../services";
 import { useRouter } from "next/navigation";
-import CoffeeSVG from "@/../public/svgs/coffee.svg"
-import Image from "next/image"
-import { Button, Input, } from "@nextui-org/react";
+import CoffeeSVG from "@/../public/svgs/coffee.svg";
+import Image from "next/image";
+import { Button, Input } from "@nextui-org/react";
 
 export const SignInForm = () => {
   const refEmail = useRef<HTMLInputElement>(null);
@@ -33,38 +33,37 @@ export const SignInForm = () => {
         <Image src={CoffeeSVG} alt="svg café xicara" />
         <h1 className="text-white font-bold text-lg letter-spacing-1">WATCH</h1>
       </div>
-      <form
-        onSubmit={handleLogin}
-        className="flex flex-col gap-2 sign-in-form"
-      >
-        <label className="font-semibold text-xs text-white" htmlFor="usernameField">
+      <form onSubmit={handleLogin} className="flex flex-col gap-2 sign-in-form">
+        <label
+          className="font-semibold text-xs text-white"
+          htmlFor="usernameField"
+        >
           Username or Email
         </label>
         <Input
           ref={refEmail}
+          value="jsmith"
           type="text"
           variant="underlined"
           color="primary"
           className="text-white"
         />
-        <label className="font-semibold text-xs mt-3 text-white" htmlFor="passwordField">
+        <label
+          className="font-semibold text-xs mt-3 text-white"
+          htmlFor="passwordField"
+        >
           Password
         </label>
         <Input
           ref={refPassword}
+          value="password"
           type="password"
           variant="underlined"
           color="primary"
           className="text-white"
         />
-        <footer
-          className="flex flex-col gap-4 mt-2"
-        >
-          <Button
-            type="submit"
-            color="primary"
-            className="mt-3 bg-purple"
-          >
+        <footer className="flex flex-col gap-4 mt-2">
+          <Button type="submit" color="primary" className="mt-3 bg-purple">
             Login
           </Button>
           <a className="text-white hover:underline" href="#">
